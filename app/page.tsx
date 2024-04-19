dbff60f224c62b4505f88c7b58df7c7763d2b21e
ACTOR-ALCHEMIST <hujiang2001@qq.com>
2024-04-19 17:15:50 +0800

    test google ad

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+import Head from 'next/head';
 import { Analytics } from "@vercel/analytics/react";
-
 import { Home } from "./components/home";
-
 import { getServerSideConfig } from "./config/server";
 
 const serverConfig = getServerSideConfig();
 
-export default async function App() {
+function App() {
   return (
     <>
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2381781395729368"
+          crossOrigin="anonymous"></script>
+      </Head>
       <Home />
       {serverConfig?.isVercel && <Analytics />}
     </>
   );
 }
+
+export default App;
