f8213faef2a87a62e40d00d5eba8767ff85ff206
ACTOR-ALCHEMIST <hujiang2001@qq.com>
2024-04-19 19:37:30 +0800

    ads debug 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2381781395729368"
-          crossOrigin="anonymous"></script>
+     crossorigin="anonymous"></script>
       </head>
       <body>{children}</body>
     </html>
